219cac70cd766554a342751dbed2a1568b2ed4d6
Dmytro Korzhov <dmytro.korzhov@gmail.com>
2023-08-22 16:13:03 +0300

    refactor and fix typos

--- app/Main.js
+++ app/Main.js
@@ -3,25 +3,23 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //* My components
+import About from "./components/About";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
-import Footer from "./components/Footer";
-import About from "./components/About";
 import Terms from "./components/Terms";
 
-function Main() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomeGuest />} />
-        <Route path="/about-us" element={<About />} />
-        <Route path="/terms" element={<Terms />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
-}
+const Main = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<HomeGuest />} />
+      <Route path="/about-us" element={<About />} />
+      <Route path="/terms" element={<Terms />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+);
 
 const root = ReactDOM.createRoot(document.querySelector("#app"));
 root.render(
--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,35 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Footer() {
-  return (
-    <footer className="border-top text-center small text-muted py-3">
-      <p>
-        <Link to="/" className="mx-1">
-          Home
-        </Link>
-        |
-        <Link className="mx-1" to="/about-us">
-          About Us
-        </Link>
-        |
-        <Link className="mx-1" to="/terms">
-          Terms
-        </Link>
-      </p>
-      <p className="m-0">
-        Copyright &copy;
-        <span> </span>
-        {new Date().getFullYear()}
-        <span> </span>
-        <Link to="/" className="text-muted">
-          ComplexApp
-        </Link>
-        .<span> </span>
-        All rights reserved.
-      </p>
-    </footer>
-  );
-}
+const Footer = () => (
+  <footer className="border-top text-center small text-muted py-3">
+    <p>
+      <Link to="/" className="mx-1">
+        Home
+      </Link>
+      |
+      <Link className="mx-1" to="/about-us">
+        About Us
+      </Link>
+      |
+      <Link className="mx-1" to="/terms">
+        Terms
+      </Link>
+    </p>
+    <p className="m-0">
+      Copyright &copy;
+      <span> </span>
+      {new Date().getFullYear()}
+      <span> </span>
+      <Link to="/" className="text-muted">
+        ComplexApp
+      </Link>
+      .<span> </span>
+      All rights reserved.
+    </p>
+  </footer>
+);
 
 export default Footer;
